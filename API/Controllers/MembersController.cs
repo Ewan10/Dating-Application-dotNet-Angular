5a0bfddcde0e6bddcf065d441dce1043e277d99d
Ioannis <secgeorgiou@hotmail.com>
2025-07-28 20:29:23 +0300

    Security implementation

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,27 +1,27 @@
 using System;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[Route("api/[controller]")]
-[ApiController]
-public class MembersController(AppDbContext context) : ControllerBase
+[Authorize]
+public class MembersController(AppDbContext context) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
     {
         var members = await context.Users.ToListAsync();
-        return Ok(members);
+        return members;
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult> GetMember(string id)
+    public async Task<ActionResult<AppUser>> GetMember(string id)
     {
         var member = await context.Users.FindAsync(id);
         if (member == null) return NotFound();
-        return Ok(member);
+        return member;
     }
 }
